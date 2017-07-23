d0a76e7fe28d3b59242edf46e859abe8979eb0ed
Your Name <wuyongwei@besttone.com.cn>
2017-07-23 13:33:32 +0800

    2c

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
+#-*- coding:utf-8 -*- 
 from flask import Flask
 from flask_script import Manager
 
 app = Flask(__name__)
 
+# 把app传递给Manager对象，以初始化Flask Script 	
 manager = Manager(app)
-
 
 @app.route('/')
 def index():
@@ -15,6 +16,8 @@ def index():
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
-
+# python hello.py runserver -h 0.0.0.0 -p 5006 
 if __name__ == '__main__':
     manager.run()
+
+
