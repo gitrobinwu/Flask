53a7c64e881e48198f3ff244aec6ac3e8d1f15bb
Your Name <wuyongwei@besttone.com.cn>
2017-07-28 10:13:04 +0800

    10a

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*- 
 from flask import render_template, abort
 from . import main
 from ..models import User
@@ -7,8 +8,10 @@ from ..models import User
 def index():
     return render_template('index.html')
 
-
+# 返回用户资料页面
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
